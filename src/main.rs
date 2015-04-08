6b9003b900d113eb2fbcacd84bbd8a1632c98136
Camille Roussel <camille@rousselfamily.com>
2015-04-07 22:09:57 -0700

    Initial req parts logging

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,21 @@ use hyper::Server;
 use hyper::server::Request;
 use hyper::server::Response;
 use hyper::net::Fresh;
+use hyper::uri::RequestUri;
 
-fn hello(_: Request, res: Response<Fresh>) {
+fn hello(req: Request, res: Response<Fresh>) {
     let mut res = res.start().unwrap();
-    res.write_all(b"Hello World!").unwrap();
+
+    let path = match req.uri {
+      RequestUri::AbsolutePath(str) => str,
+      _ => "".to_string()
+    };
+
+    println!("path: {}", path);
+    println!("remoteAddy: {}", req.remote_addr);
+    println!("method: {}", req.method);
+    
+    res.write_all(b"Hello").unwrap();
     res.end().unwrap();
 }
 
